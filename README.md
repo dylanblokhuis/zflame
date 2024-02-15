079a3f211296e00a95a0a91eb22caa6ab0becc7f
dylanblokhuis <dylanblokhuis3@hotmail.com>
2024-02-15 16:30:13 +0100

    readme

---
+++ README.md
@@ -0,0 +1,3 @@
+notes:
+
+``zig build run --prominent-compile-errors --summary none`` is cool
--- src/main.zig
+++ src/main.zig
@@ -1,8 +1,6 @@
 const std = @import("std");
 const game = @import("./game.zig");
 
-fn henkie() !void {}
-
 pub fn main() !void {
     // Prints to stderr (it's a shortcut based on `std.io.getStdErr()`)
     std.debug.print("All your {s} are belong to us.\n", .{"codebase"});
